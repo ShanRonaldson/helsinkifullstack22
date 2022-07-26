66838a55f956a2592e6e11c18e4a5bf01e7a8ac4
Shannon Schrauwen <63632791+ShanSchrauwen@users.noreply.github.com>
2022-07-26 23:09:34 +0300

    part 2b complete
    Phonebook part 2 b complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,23 +2,50 @@ import { useState } from 'react';
 import { Add } from './components/Add';
 import { Header } from './components/Header'
 import { List } from './components/List';
+import { Search } from './components/Search';
 
 export const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '123 123 123' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
 
-  const [newName, setNewName] = useState({ name: '' })
+  const [newName, setNewName] = useState({ name: '', number: '' });
+
+  const [search, setSearch] = useState({ name: '' });
+
+  const [filteredPersons, setFilteredPersons] = useState({});
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setPersons(persons.concat(newName));
-    setNewName({ name: '' })
+
+    persons.map(person => {
+      if (newName.name === person.name) {
+        alert(`${newName.name} is already added to the phonebook`);
+        return true;
+      } else {
+        setPersons(persons.concat(newName));
+      }
+    });
+    setNewName({ name: '', number: '' })
   }
 
   const handleChange = (event) => {
-    setNewName({ name: event.target.value })
+    setNewName({ ...newName, [event.target.id]: event.target.value })
+  }
+
+  const handleSearch = (event) => {
+    setSearch({ name: event.target.value.toLowerCase() })
+
+    const personsArr = persons.filter(person => {
+      return person.name.toLowerCase().match(new RegExp(search.name, 'g'))
+    })
+
+    setFilteredPersons(personsArr)
+
   }
 
   return (
@@ -26,6 +53,8 @@ export const App = () => {
       <Header text={'Phonebook'} />
 
       <Add newName={newName} handleSubmit={handleSubmit} handleChange={handleChange} />
+
+      <Search handleSearch={handleSearch} filter={filteredPersons} />
 
       <List persons={persons} />
     </>
--- part2/phonebook/src/components/List.jsx
+++ part2/phonebook/src/components/List.jsx
@@ -4,11 +4,15 @@ export const List = ({ persons }) => {
 
     return (
         <>
-            <Header text={'People'} />
+            <Header text={'People'} level={'h2'} />
 
             <ul>
-                {persons.map(person => (
-                    <li key={person.name}>{person.name}</li>
+                {persons.map((person, id) => (
+                    <li key={id}>
+                        <span>{person.name }</span>
+                        <span>{' '}</span>
+                        <span>{person.number}</span>
+                    </li>
                 ))}
             </ul>
         </>
