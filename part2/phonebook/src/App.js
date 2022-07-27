bcb50e786400540bbb0559d153b3f39ab465b4a3
Shannon Schrauwen <63632791+ShanSchrauwen@users.noreply.github.com>
2022-07-27 22:15:29 +0300

    part 2c phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Add } from './components/Add';
 import { Header } from './components/Header'
 import { List } from './components/List';
@@ -6,12 +7,13 @@ import { Search } from './components/Search';
 
 export const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123 123 123' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
 
   const [newName, setNewName] = useState({ name: '', number: '' });
 
